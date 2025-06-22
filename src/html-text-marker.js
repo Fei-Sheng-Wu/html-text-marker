844d91b96f74c9f99c95e193847d2c5a878e52b5
FeiShengWu <36951947+Fei-Sheng-Wu@users.noreply.github.com>
2025-06-22 12:14:25 -0600

    Enhancements

--- src/html-text-marker.js
+++ src/html-text-marker.js
@@ -1,4 +1,28 @@
-function thCustomizeAll(element, text, format, options) {
+/**
+ * @typedef {Object} HtmOptions
+ * @property {string[]} whitelist - the list of accepted element tag names in uppercase, CSS selectors, or * to represent all elements
+ * @property {string[]} blacklist - the list of unaccepted element tag names in uppercase, CSS selectors, or * to represent all elements
+ * @property {(element:HTMLElement) => boolean} selector - the custom predicate to determine whether an element is accepted
+ */
+
+/**
+ * The default options provided by html-text-marker.
+ * @type {HtmOptions}
+ */
+const htmDefaultOptions = {
+    whitelist: ['*'],
+    blacklist: [],
+    selector: undefined
+};
+
+/**
+ * Update all occurances of the provided text with a custom format.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {string} format - the custom format to update the text to, where {0} is replaced by the text
+ * @param {Object} options - the custom options to follow
+ */
+function htmCustomizeAll(element, text, format, options) {
     for (var child = element.firstChild, childNext; child !== null; child = childNext) {
         childNext = child.nextSibling;
 
@@ -32,30 +56,67 @@ function thCustomizeAll(element, text, format, options) {
             continue;
         }
 
-        thCustomizeAll(child, text, format, options);
+        htmCustomizeAll(child, text, format, options);
     }
 }
 
-function thHiglightAll(element, text, options) {
-    thCustomizeAll(element, text, '<mark>{0}</mark>', options);
+/**
+ * Highlight all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {Object} options - the custom options to follow
+ */
+function htmHiglightAll(element, text, options) {
+    htmCustomizeAll(element, text, '<mark>{0}</mark>', options);
 }
 
-function thBoldAll(element, text, options) {
-    thCustomizeAll(element, text, '<b>{0}</b>', options);
+/**
+ * Bold all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {Object} options - the custom options to follow
+ */
+function htmBoldAll(element, text, options) {
+    htmCustomizeAll(element, text, '<b>{0}</b>', options);
 }
 
-function thItalicizeAll(element, text, options) {
-    thCustomizeAll(element, text, '<i>{0}</i>', options);
+/**
+ * Italicize all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {Object} options - the custom options to follow
+ */
+function htmItalicizeAll(element, text, options) {
+    htmCustomizeAll(element, text, '<i>{0}</i>', options);
 }
 
-function thUnderlineAll(element, text, options) {
-    thCustomizeAll(element, text, '<u>{0}</u>', options);
+/**
+ * Underline all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {Object} options - the custom options to follow
+ */
+function htmUnderlineAll(element, text, options) {
+    htmCustomizeAll(element, text, '<u>{0}</u>', options);
 }
 
-function thLinkAll(element, text, link, options) {
-    thCustomizeAll(element, text, '<a href="' + link + '">{0}</a>', options);
+/**
+ * Hyperlink all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {string} link - the link to use
+ * @param {Object} options - the custom options to follow
+ */
+function htmLinkAll(element, text, link, options) {
+    htmCustomizeAll(element, text, '<a href="' + link + '">{0}</a>', options);
 }
 
-function thDeleteAll(element, text, options) {
-    thCustomizeAll(element, text, '', options);
+/**
+ * Delete all occurances of the provided text.
+ * @param {HTMLElement} element - the root element to find the provided text in
+ * @param {string} text - the text to match with
+ * @param {Object} options - the custom options to follow
+ */
+function htmDeleteAll(element, text, options) {
+    htmCustomizeAll(element, text, '', options);
 }
